f5377a09496b3e33b5acb76abddec77c25c9aadb
Bonsung Koo <koo@pjfactory.com>
2020-12-02 23:33:48 +0900

    SPM 문서 수정

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EnergyBar",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EnergyBar",
-            dependencies: []),
+            path: "energybar"),
         .testTarget(
             name: "EnergyBarTests",
             dependencies: ["energybarTests"]),
