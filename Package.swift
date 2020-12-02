f8001e79f7f2c4490f8e02f0b6063c4e74d144af
Bonsung Koo <koo@pjfactory.com>
2020-12-02 23:37:43 +0900

    SPM 문서 수정

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "EnergyBar",
+            name: "energybar",
             targets: ["energybar"]),
     ],
     dependencies: [
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "EnergyBar",
+            name: "energybar",
             path: "energybar"),
         .testTarget(
-            name: "EnergyBarTests",
+            name: "energybarTests",
             dependencies: ["energybarTests"]),
     ]
 )
